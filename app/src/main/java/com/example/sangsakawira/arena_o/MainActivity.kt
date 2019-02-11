1f1e98932538ed090069ccb8da7a30b50acaf688
Sangsaka Wira <sangsakawira@gmail.com>
2019-02-11 11:35:35 +0700

    Frist Commit, Baru ada Login sama Register

---
+++ app/src/main/java/com/example/sangsakawira/arena_o/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.sangsakawira.arena_o
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
